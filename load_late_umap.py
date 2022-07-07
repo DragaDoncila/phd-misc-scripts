ef2be5a79014ef1b17a5d03120a3588a5cc7ea06
doncilapopd <doncilapopd@saalfelds-ws4.hhmi.org>
2022-07-07 17:49:00 -0400

    Update scale to crazy numbers

--- load_late_umap.py
+++ load_late_umap.py
@@ -2,15 +2,16 @@ import napari
 import dask.dataframe as df
 
 STACKED_NODES = '/home/doncilapopd@hhmi.org/phd-misc-scripts/140521_late/140521_late_te_all_tracks_stacked.csv'
-UMAP_NODES = '/home/doncilapopd@hhmi.org/phd-misc-scripts/140521_late/140521_late_te_all_tracks_umap.csv'
-TRACKS = '/home/doncilapopd@hhmi.org/phd-misc-scripts/140521_late/140521_late_te_all_tracks_napari.csv'
+TRACKS = '/home/doncilapopd@hhmi.org/phd-misc-scripts/140521_late/140521_late_te_all_tracks_with_umap.csv'
 
 viewer = napari.Viewer()
-viewer.open(STACKED_NODES, layer_type='points', size=20)
 
-tracks_data = df.read_csv(TRACKS)[['track_id', 't', 'z', 'y', 'x']]
-viewer.add_tracks(tracks_data.to_dask_array(lengths=True))
-viewer.open(UMAP_NODES, layer_type='points', size=1, visible=False)
+tracks_data = df.read_csv(TRACKS)[['track_id', 't', 'umap_y', 'umap_x']]
+tracks_data['umap_y'] = tracks_data['umap_y'].mul(50)
+tracks_data['umap_x'] = tracks_data['umap_x'].mul(50)
+viewer.add_tracks(tracks_data, scale=(100, 1, 1))
 
+points_data = tracks_data[['t', 'umap_y', 'umap_x']]
+viewer.add_points(points_data, size=2, scale=(100, 1, 1))
 
 napari.run()
